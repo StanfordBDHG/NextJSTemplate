2717799bf69f3f9fec2352a5e5fcbe027d8c124d
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-10-26 20:06:30 -0700

    Fix REUSE

--- app/page.test.tsx
+++ app/page.test.tsx
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health TypeScript Template open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 import React from 'react'
 import { render } from '@testing-library/react'
 import '@testing-library/jest-dom'
