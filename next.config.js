cbda3849a367e4e78368ea9093b109233271a9a1
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-10-27 12:39:50 -0700

    Add GitHub Action Deployment (#4)
    # Add GitHub Action Deployment
    
    ## :gear: Release Notes 
    - Adds GitHub Action Deployment
    
    
    ### Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).

--- next.config.js
+++ next.config.js
@@ -8,9 +8,20 @@
 
 /** @type {import('next').NextConfig} */
 
+const output = process.env.NEXT_JS_OUTPUT || 'standalone'
+const imagesUnoptimized = process.env.NEXT_JS_IMAGES_UNOPTIMIZED == 'true'
+const basePath = process.env.NEXT_JS_BASE_PATH ?? ''
+
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
+  output: output,
+  images: {
+    unoptimized: imagesUnoptimized,
+  },
+  basePath: basePath,
+  env: {
+    basePath: basePath,
+  },
 }
 
 module.exports = nextConfig
