3ec3e5c1b502588db95f17baabb703f76b7c9847
Vishnu Ravi <vishnur@stanford.edu>
2023-11-11 09:54:50 -0500

    Fix REUSE error

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
+//
+// This source file is part of the Stanford Biodesign Digital Health Next.js Template open-source project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University and the project authors (see CONTRIBUTORS.md)
+//
+// SPDX-License-Identifier: MIT
+//
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
